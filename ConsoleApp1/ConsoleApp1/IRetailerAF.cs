0db1ee21f3c29d05ca682e82af668357e70de088
BrunoBrckovic <bruno.brckovic@gmail.com>
2018-05-30 01:58:25 +0200

    Gotovo

---
+++ ConsoleApp1/ConsoleApp1/IRetailerAF.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class IRetailerAF
+    {
+        protected abstract ComputersCF FindPC();
+        protected abstract DisplaysCF FindDisplay();
+
+        public void GetPC()
+        {
+            var pc = FindPC();
+            var display = FindDisplay();
+            Console.WriteLine(this.GetType().Name + " sell " + pc.GetType().Name + " PC's and they use " + display.GetType().Name.Substring(1) + "'' display");
+        }
+    }
+
+
+    public class LINKS : IRetailerAF
+    {
+        protected override ComputersCF FindPC()
+        {
+            return new ASUS();
+        }
+
+        protected override DisplaysCF FindDisplay()
+        {
+            return new _19();
+            //return new _28();
+        }
+    }
+
+    public class KING : IRetailerAF
+    {
+        protected override ComputersCF FindPC()
+        {
+            return new ACER();
+        }
+
+        protected override DisplaysCF FindDisplay()
+        {
+            return new _28();
+        }
+    }
+
+    public class MSAN : IRetailerAF
+    {
+        protected override ComputersCF FindPC()
+        {
+            return new TOSHIBA();
+        }
+
+        protected override DisplaysCF FindDisplay()
+        {
+            return new _19();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        enum SELLER
+        {
+            LINKS,
+            KING,
+            MSAN
+        }
+        class SellerChecker
+        {
+            IRetailerAF ret;
+            SELLER manu;
+
+            public void CheckProducts(SELLER sell)
+            {
+                switch (sell)
+                {
+                    case SELLER.LINKS:
+                        ret = new LINKS();
+                        break;
+                    case SELLER.KING:
+                        ret = new KING();
+                        break;
+                    case SELLER.MSAN:
+                        ret = new MSAN();
+                        break;
+                }
+                ret.GetPC();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            SellerChecker checker = new SellerChecker();
+            checker.CheckProducts(SELLER.LINKS);
+            checker.CheckProducts(SELLER.KING);
+            Console.ReadLine();
+
+            IRetailerAF ret1 = new LINKS();
+            ret1.GetPC();
+            Console.ReadLine();
+
+            IRetailerAF ret2 = new KING();
+            ret2.GetPC();
+            Console.ReadLine();
+
+            IRetailerAF ret3 = new MSAN();
+            ret3.GetPC();
+            Console.ReadLine();
+        }
+    }
+}
